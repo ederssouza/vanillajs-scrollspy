ca822774a11aa06007cee106d7f928a42355ab69
Eder Sampaio <edersampaio@outlook.com.br>
2017-08-23 13:35:22 -0300

    changing method

--- src/js/scrollspy.js
+++ src/js/scrollspy.js
@@ -2,7 +2,7 @@ window.requestAnimFrame = (function(){
   return  window.requestAnimationFrame       ||
           window.webkitRequestAnimationFrame ||
           window.mozRequestAnimationFrame    ||
-          function( callback ){
+          function(callback){
             window.setTimeout(callback, 1000 / 60);
           };
 })();
@@ -10,7 +10,7 @@ window.requestAnimFrame = (function(){
 
 /**
  *  @param {number} scrollTargetY
- *  @param {number} [speed] - scroll speed, default value 1500
+ *  @param {number} [speed]  - scroll speed, default value 1500
  *  @param {string} [easing] - scroll type 'easeOutSine', 'easeInOutSine' or 'easeInOutQuint', default value easeInOutQuint
  */
 
@@ -26,6 +26,7 @@ function scrollToY(scrollTargetY, speed, easing) {
   var time = Math.max(0.1, Math.min(Math.abs(scrollY - scrollTargetY) / speed, 0.8));
 
   var easingEquations = {
+
     easeOutSine: function (pos) {
       return Math.sin(pos * (Math.PI / 2));
     },
@@ -66,13 +67,13 @@ function scrollToY(scrollTargetY, speed, easing) {
 
 function menuControl(menu) {
 
-  var scrollPos = window.pageYOffset;
-  var links = menu.querySelectorAll('a[href^="#"]');
+  var scrollPos = window.scrollY || document.documentElement.scrollTop,
+      links     = menu.querySelectorAll('a[href^="#"]');
 
   for(var i = 0; i < links.length; i++) {
 
-    var currLink = links[i];
-    var refElement = document.querySelector(currLink.getAttribute('href'));
+    var currLink   = links[i],
+        refElement = document.querySelector(currLink.getAttribute('href'));
 
     if(refElement.offsetTop <= scrollPos && (refElement.offsetTop + refElement.clientHeight) > scrollPos) {
       currLink.classList.add('active');
@@ -85,17 +86,17 @@ function menuControl(menu) {
 
 
 /**
- *  @param {object} menu - menu selector
- *  @param {number} [speed] - scroll speed, default value 1500
+ *  @param {object} menu     - menu selector
+ *  @param {number} [speed]  - scroll speed, default value 1500
  *  @param {string} [easing] - scroll type 'easeOutSine', 'easeInOutSine' or 'easeInOutQuint', default value easeInOutQuint
  */
 
 function animated(menu, speed, easing) {
 
-
   var i, links = menu.querySelectorAll('a[href^="#"]');
 
   function control(e) {
+
     e.preventDefault();
 
     var target = document.querySelector(this.hash);
@@ -111,8 +112,8 @@ function animated(menu, speed, easing) {
 
 
 /**
- *  @param {object} menu - menu selector
- *  @param {number} [speed] - scroll speed, default value 1500
+ *  @param {object} menu     - menu selector
+ *  @param {number} [speed]  - scroll speed, default value 1500
  *  @param {string} [easing] - scroll type 'easeOutSine', 'easeInOutSine' or 'easeInOutQuint', default value easeInOutQuint
  */
 
